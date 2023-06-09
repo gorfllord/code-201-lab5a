2c8dcdf3fb2e590f3bd7a5d24a455f8ee3a08ab1
Geoff Lloyd <orionblack@gmail.com>
2023-06-09 14:43:27 -0700

    Misread question 3, fixing errors now. partial question 4 code included.

--- js/app.js
+++ js/app.js
@@ -50,11 +50,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  let sumResult = a + b + c;
-  let productResult = a * b * c;
-  return [sumResult, productResult, `${a} and ${b} and ${c} sum to ${sumResult}.`, `The product of ${a} and ${b} and ${c} is ${productResult}.`]
+  let sumResult = sum(a, b)[0];
+  sumResult = sum(sumResult, c)[0];
+  let productResult = multiply(a, b)[0];
+  productResult = multiply(productResult, c)[0];
+  return [sumResult, productResult, `${a} and ${b} and ${c} sum to ${sumResult}.`, `The product of ${a} and ${b} and ${c} is ${productResult}.`];
 }
-
+console.log(sumAndMultiply(4, 7, 5));
 // Here is the test for sumAndMultiply(); uncomment it to run it
 testSumAndMultiply(4,7,5);
 
@@ -74,12 +76,16 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let sumResult = 0;
+  for (let i = 0; i < sumArr.length; i++) {
+    sumResult = sum(sumResult, sumArr[i]);
+  }
+  return [sumResult, `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumResult} is their sum.`];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
